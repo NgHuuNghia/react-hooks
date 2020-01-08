83e38b87a735a15fb945572192311cde671c9257
nguyenhuunghia <nguyenhuunghia152@gmail.com>
2020-01-08 14:38:34 +0700

    update use spiliting-code with react.lazy()

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import NavBar from './components/Navbar';
-import BookList from './components/BookList';
+import React, { Suspense, lazy } from 'react';
+import Loading from './components/shared/loading';
+//context
 import ThemeContextProvider from './contexts/themeContext';
-import ThemeToggle from './components/themeToggle';
 import AutherContextProvider from './contexts/authContext';
 import BookContextProvider from './contexts/bookContext';
+//component
+const NavBar = lazy( () => import('./components/Navbar'));
+const BookList = lazy( () => import('./components/BookList'));
+const ThemeToggle = lazy( () => import('./components/themeToggle')); 
+
 
 function App() {
   return (
     <div className="App">
-      <AutherContextProvider>
-        <ThemeContextProvider>
-          <ThemeToggle />
-          <NavBar />
-          <BookContextProvider>
-            <BookList />
-          </BookContextProvider>
-        </ThemeContextProvider>
-      </AutherContextProvider>
+      <Suspense fallback = { <Loading/> }>
+        <AutherContextProvider>
+          <ThemeContextProvider>
+            <ThemeToggle />
+            <NavBar />
+            <BookContextProvider>
+              <BookList />
+            </BookContextProvider>
+          </ThemeContextProvider>
+        </AutherContextProvider>
+      </Suspense>
+
     </div>
   );
 }
